3369f37e2cf8aa43a8b513c6103e0da11c8b9175
Gautier Benoit <gautier.benoit@etu.univ-cotedazur.fr>
2024-01-26 23:18:04 +0100

    Update assignment detail component styles and functionality

--- src/app/assignments/edit-assignment/edit-assignment.component.ts
+++ src/app/assignments/edit-assignment/edit-assignment.component.ts
@@ -13,6 +13,8 @@ export class EditAssignmentComponent implements OnInit {
   assignment!: Assignment | undefined;
   nomAssignment!: string;
   dateDeRendu!: Date;
+  detail!: string;
+  note!: number;
 
   constructor(
     private assignmentsService: AssignmentsService,
@@ -21,26 +23,27 @@ export class EditAssignmentComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    //this.getAssignment();
+    this.getAssignment();
 
     // affichage console des query params et du fragment
     console.log("Query params : ", this.route.snapshot.queryParams);
     console.log("Fragment : ", this.route.snapshot.fragment);
   }
 
-  /**getAssignment() {
+  getAssignment() {
     // on récupère l'id dans le snapshot passé par le routeur
     // le "+" force l'id de type string en "number"
     const id = +this.route.snapshot.params['id'];
 
-    this.assignmentsService.getAssignment(id).subscribe((assignment) => {
-      if (!assignment) return;
-      this.assignment = assignment;
+    this.assignmentsService.getAssignment().subscribe((assignments: Assignment[]) => {
+      if (!assignments || assignments.length === 0) return;
+      this.assignment = assignments[0];
       // Pour pré-remplir le formulaire
       this.nomAssignment = assignment.nom;
       this.dateDeRendu = assignment.dateDeRendu;
     });
   }
+
   onSaveAssignment() {
     if (!this.assignment) return;
 
@@ -49,15 +52,12 @@ export class EditAssignmentComponent implements OnInit {
     this.assignment.dateDeRendu = this.dateDeRendu;
     this.assignmentsService
       .updateAssignment(this.assignment)
-      .subscribe((message) => {
+      .subscribe((message: string) => {
         console.log(message);
 
         // navigation vers la home page
         this.router.navigate(['/home']);
       });
-
-  */
+  }
   
 }
-
-
